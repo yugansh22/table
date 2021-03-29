63e19f236b01ac0c0d47e11111f7154c2719b1fe
yugansh <sneharts21@gmail.com>
2021-03-29 11:43:45 +0530

    removed console.log

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function printtable(number){
     for(var i=1;i<=10;i++){
        
         table = table +  number +" X "+ i + " = " + number*i + "<br>";
-        console.log(table);
     } 
     
     document.getElementById("out").innerHTML = table;
